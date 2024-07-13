e7cef9a9a84adc2eab50a2411591a7b0c8b8ee59
accelada <adriancelada@gmail.com>
2024-07-13 18:18:17 +0200

    [UPDATE][GENERAL][Challenge 2 reviewed and resolved]

--- main.py
+++ main.py
@@ -11,7 +11,6 @@
 
 # Comments:
 # Execute this code to see the solution of both exercises.
-# Note: challenge 2 is not properly resolved.
 
 #Challenge 1 - Algorithms
 #-------------------------------------------------------------------------------------------------------
@@ -57,39 +56,98 @@ def longest_common_consecutive_substring(A, B, C):
 # subsets.
 
 def count_equal_color_subsets(arr):
-    n = len(arr)
     
-    count_b = count_g = count_r = 0
-    prefix_count = {(0, 0, 0): 1}  
+    array_length = len(arr)
     
-    total_valid_splits = 0
-    
-    for i in range(n):
-        if arr[i] == 'b':
-            count_b += 1
-        elif arr[i] == 'g':
-            count_g += 1
-        elif arr[i] == 'r':
-            count_r += 1
+    # ODD LENGTH
+    if array_length % 2 != 0:
+        if len(set(arr)) == 1:
+            return array_length + 1
         
-        current_prefix = (count_b, count_g, count_r)
-        
-        if current_prefix in prefix_count:
-            total_valid_splits += prefix_count[current_prefix]
-            prefix_count[current_prefix] += 1
         else:
-            prefix_count[current_prefix] = 1
+            return 0
     
-    return total_valid_splits
+    # EVEN LENGTH
+    else:
+        unique_elements = list(set(arr))
+        num_unique_elements = len(unique_elements)
+        elements = elements_counter(arr, unique_elements)
+        
+        num_of_common_subsets = 0
+        
+        for i in range(array_length):
+            ii = i + 1
+            if array_length % ii == 0:
+                divider = int(array_length / ii)
+                list_of_sub_sets = []
+                
+                for j in range(ii):
+                    list_of_sub_sets.append(arr[divider*j:divider*(j+1)])
+                
+                if ii == 1:
+                    if check_same_elements(elements):
+                        num_of_common_subsets += ii
+                    
+                else:
+                    same_elements_in_each_sub_set = True
+                    for k, sub_set in enumerate(list_of_sub_sets):
+                        if k != 0:
+                            if elements_counter(sub_set, elements) != elements_counter(list_of_sub_sets[k-1], elements):
+                                same_elements_in_each_sub_set = False
+                                break
+                    
+                    if same_elements_in_each_sub_set:        
+                        num_of_common_subsets += ii
+                            
+            else:
+                continue
+                    
+        return num_of_common_subsets
+    
+def check_same_elements(elements):
+    keys = list(elements.keys())
+    first_val = elements[keys[0]]
+    
+    return all(elements[key] == first_val for key in keys)
+    
+def elements_counter(string, items):
+    # Dict initialization
+    elements = dict()
+    
+    for item in items:
+        elements[item] = 0
+    
+    for s in string:
+        elements[s] += 1
+        
+    return elements
+
 
 if __name__ == "__main__":
     # Challenge 1
+    # Example 1
+    A = "ADDB"
+    B = "CDDE"
+    C = "EDDF"
+    sequence = longest_common_consecutive_substring(A,B,C)
+    print(f"Output challenge 1 - example 1: {sequence}")
+    
+    # Example 2
     A = "UIBAZDBSIAHFB"
     B = "PQACIZDBIBDLAG"
     C = "QIDBCZDBKSHDVF"
     sequence = longest_common_consecutive_substring(A,B,C)
-    print(f"Output challenge 1: {sequence}")
+    print(f"Output challenge 1 - example 2: {sequence}")
     
     # Chanllenge 2
+    # Example 1
+    colors = ['r', 'r', 'b', 'b', 'g', 'g']
+    print(f"Output challenge 2 - example 3: {count_equal_color_subsets(colors)}")
+    
+    # Example 2
+    colors = ['r', 'r', 'b', 'b', 'g']
+    print(f"Output challenge 2 - example 3: {count_equal_color_subsets(colors)}")
+    
+    # Example 3
     colors = ['r', 'b', 'g', 'g', 'b', 'r']
-    print(f"Output challenge 2: {count_equal_color_subsets(colors)}")
+    print(f"Output challenge 2 - example 3: {count_equal_color_subsets(colors)}")
